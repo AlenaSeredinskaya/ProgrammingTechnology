de06172fd59fe3b09790ee32072eb28d96f7bb5c
Alena <seredinskaaalena5@gmail.com>
2025-02-20 10:11:21 +0700

    Add files via upload

---
+++ IntValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lesson0.Validators
+{
+    class IntValidator
+    {
+        public static bool Validate(int value)
+        {
+            return value > 0;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lesson0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new(161, 50, DateTime.Parse("15.12.2004"), "Mark", "Winston");
+            Console.WriteLine(person);
+
+            person.ChangeHeight(166);
+            Console.WriteLine(person);
+      
+        }
+    }
+}
