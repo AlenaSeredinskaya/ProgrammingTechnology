e269a3bba76823b5b888c1b6b961c27ef7640a74
Alena <seredinskaaalena5@gmail.com>
2025-03-06 10:55:36 +0700

    Add files via upload

---
+++ lesson2/GiftCardAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lesson2.Base;
+
+namespace lesson2
+{
+    class GiftCardAccount : BankAccount
+    {
+        public GiftCardAccount(string owner, decimal initialBalance)
+            : base(owner, initialBalance)
+        {
+
+        }
+
+        public override void PerformMonthEndTransactions()
+        {
+            MakeWithdrawal(-Balance, DateTime.Now, ":(");
+        }
+            
+
+    }
+}
