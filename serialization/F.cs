81520533f3d6649611112338abdb13b587abd8d9
Alena <seredinskaaalena5@gmail.com>
2025-09-19 07:58:39 +0700

    Update F.cs

--- serialization/F.cs
+++ serialization/F.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -13,6 +13,13 @@ namespace serialization
         public int i3 { get; set; }
         public int i4 { get; set; }
         public int i5 { get; set; }
+        public int[] mas;
+
+        public F()
+        {
+         i1=1; i2=2; i3=3 i4=4; i5=5;
+         mas = new int[] { 1, 2, 3, 4, 5};
+        }
 
         public static F Get() => new F() { i1=1, i2=2, i3=3, i4=4, i5=5 };
     }
