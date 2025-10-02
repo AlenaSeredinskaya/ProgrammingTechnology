de323f58cc9fa95551ead59d2924e20bef9d677f
Alena <seredinskaaalena5@gmail.com>
2025-10-02 12:11:28 +0700

    Add files via upload

---
+++ Otus.Task1/Models/GameSaveFile.cs
@@ -0,0 +1,141 @@
+﻿using ProtoBuf;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace Otus.Task1.Models
+{
+    public enum Gender
+    {
+        None = 0,
+        Male = 1,
+        Female = 2,
+    }
+
+    // Конвертер для JSON (enum как m/f)
+    public class GenderJsonConverter : JsonConverter<Gender>
+    {
+        public override Gender Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            string value = reader.GetString();
+            return value switch
+            {
+                "m" => Gender.Male,
+                "f" => Gender.Female,
+                _ => Gender.None
+            };
+        }
+
+        public override void Write(Utf8JsonWriter writer, Gender value, JsonSerializerOptions options)
+        {
+            string result = value switch
+            {
+                Gender.Male => "m",
+                Gender.Female => "f",
+                _ => ""
+            };
+            writer.WriteStringValue(result);
+        }
+    }
+
+    [Serializable]
+    [ProtoContract]
+    public class Item
+    {
+        private int _quantity;
+
+        [ProtoMember(1)]
+        public string Name { get; set; }
+
+        [ProtoMember(2)]
+        public int Quantity
+        {
+            get => _quantity;
+            set => _quantity = value;
+        }
+    }
+
+    [Serializable]
+    [ProtoContract]
+    public class User
+    {
+        [XmlAttribute("level")]
+        [ProtoMember(1)]
+        public int Level { get; set; }
+
+        [ProtoMember(2)]
+        public string Name { get; set; }
+
+        [ProtoIgnore]
+        [JsonIgnore]
+        [ProtoMember(3)]
+        public Gender Gender { get; set; }
+
+        // JSON - сериализация через конвертер
+        [JsonPropertyName("gender")]
+        [JsonConverter(typeof(GenderJsonConverter))]
+        public Gender JsonGender
+        {
+            get => Gender;
+            set => Gender = value;
+        }
+
+        // XML - сериализация как m/f
+        [XmlElement("gender")]
+        [JsonIgnore]
+        public string XmlGender
+        {
+            get => Gender switch
+            {
+                Gender.Male => "m",
+                Gender.Female => "f",
+                _ => ""
+            };
+            set
+            {
+                Gender = value switch
+                {
+                    "m" => Gender.Male,
+                    "f" => Gender.Female,
+                    _ => Gender.None
+                };
+            }
+        }
+    }
+
+    [Serializable]
+    [ProtoContract]
+    [ProtoInclude(100, typeof(SaveFile))]
+    public class GameStatus
+    {
+        [ProtoMember(1)]
+        public string CurrentLocation { get; set; }
+
+        [JsonPropertyName("u")]
+        [XmlElement("u")]
+        [ProtoMember(2)]
+        public User User { get; set; }
+
+        [ProtoMember(3)]
+        public Item[] Items { get; set; }
+
+        [XmlIgnore]
+        [JsonIgnore]
+        [ProtoIgnore] 
+        public (double, double) Coords { get; set; }
+    }
+
+    [ProtoContract]
+    public class SaveFile : GameStatus
+    {
+        [ProtoMember(4)]
+        public DateTime CreatedDate { get; set; }
+
+        [ProtoMember(5)]
+        public DateTime? SaveDate { get; set; }
+
+        [ProtoMember(6)]
+        public string FileName { get; set; }
+    }
+}
---
+++ Otus.Task1/Program.cs
@@ -0,0 +1,136 @@
+﻿using Otus.Task1.Models;
+using ProtoBuf;
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+class Program
+{
+    static SaveFile Generate1()
+    {
+        var res = new SaveFile();
+        res.Coords = (1241.44, 124145.4);
+        res.CurrentLocation = "Dungeon";
+        res.User = new User { Level = 10, Name = "Пушкин", Gender = Gender.Male };
+        res.Items = new[] { new Item() { Name = "Топор", Quantity = 2 } };
+        res.CreatedDate = DateTime.Now;
+        res.SaveDate = DateTime.Now;
+        res.FileName = "SaveGame1";
+        return res;
+    }
+
+    static SaveFile Generate2()
+    {
+        var res = new SaveFile();
+        res.Coords = (121.44, 124.4);
+        res.CurrentLocation = "Subway";
+        res.User = new User { Level = 10, Name = "Feodorov", Gender = Gender.Female };
+        res.Items = new[] { new Item() { Name = "Stick", Quantity = -2 } }; // вызовет exception
+        res.CreatedDate = DateTime.Now;
+        res.SaveDate = null;
+        res.FileName = "SaveGame2";
+        return res;
+    }
+
+    /// <summary>
+    /// Бинарная сериализация
+    /// </summary>
+    /// <param name="sf"></param>
+    /// <exception cref="Exception"></exception>
+    static void SerializeBinary(SaveFile sf)
+    {
+        foreach (var item in sf.Items)
+        {
+            if (item.Quantity < 0)
+                throw new Exception("Количество не может быть < 0.");
+        }
+
+        using (FileStream fs = new FileStream($"{sf.FileName}.bin", FileMode.Create))
+        {
+            Serializer.Serialize(fs, sf);
+        }
+    }
+
+    /// <summary>
+    /// Сериализация JSON
+    /// </summary>
+    /// <param name="sf"></param>
+    /// <exception cref="Exception"></exception>
+    static void SerializeJson(SaveFile sf)
+    {
+        foreach (var item in sf.Items)
+        {
+            if (item.Quantity < 0)
+                throw new Exception("Количество не может быть < 0.");
+        }
+
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+
+        var json = JsonSerializer.Serialize(sf, options);
+        File.WriteAllText($"{sf.FileName}.json", json);
+    }
+
+    /// <summary>
+    /// Сериализация XML
+    /// </summary>
+    /// <param name="sf"></param>
+    /// <exception cref="Exception"></exception>
+    static void SerializeXml(SaveFile sf)
+    {
+        foreach (var item in sf.Items)
+        {
+            if (item.Quantity < 0)
+                throw new Exception("Количество не может быть < 0.");
+        }
+
+        var serializer = new XmlSerializer(typeof(SaveFile));
+        using (FileStream fs = new FileStream($"{sf.FileName}.xml", FileMode.Create))
+        {
+            try
+            {
+                serializer.Serialize(fs, sf);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка сериализации XML {ex.Message}");
+            }
+        }
+    }
+
+    static SaveFile DeserializeBinary(string filePath)
+    {
+        using (FileStream fs = new FileStream(filePath, FileMode.Open))
+        {
+            return Serializer.Deserialize<SaveFile>(fs);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        var g1 = Generate1();
+        var g2 = Generate2();
+
+        try
+        {
+            SerializeBinary(g1);
+            SerializeJson(g1);
+            SerializeXml(g1);
+
+            var loadedG1 = DeserializeBinary($"{g1.FileName}.bin");
+            Console.WriteLine($"Десериализованный: Location={loadedG1.CurrentLocation}, User={loadedG1.User.Name}, Level={loadedG1.User.Level}");
+
+            SerializeBinary(g2); 
+            SerializeJson(g2);
+            SerializeXml(g2);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
